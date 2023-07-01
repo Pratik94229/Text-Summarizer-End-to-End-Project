453d8ad2ab9902b8fdbd2976c77bc8837176345c
Pratik94229 <pratik.941@gmail.com>
2023-07-01 17:11:47 +0530

    data ingestion completed

--- template.py
+++ template.py
@@ -10,7 +10,7 @@ project_name = "textSummarizer"
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
