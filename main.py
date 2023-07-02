6075c76724c752efc0f76b884502fcd58bd592f7
Pratik94229 <pratik.941@gmail.com>
2023-07-02 19:20:44 +0530

    data validation completed

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
